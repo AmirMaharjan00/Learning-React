983c9ddcc56728e12f6318e0077b6fec67d03ddb
Amir Maharjan <mhrznamir.am@gmail.com>
2023-10-28 15:51:12 +0545

    adding images and css

--- react-official/src/Components/Home.js
+++ react-official/src/Components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Astronaut from '../Assets/astronaut.jpg'
 
 export default function Home () {
     return (
         <>
-            <h1>Welcome To React Official</h1>
+            <div className='portfolio'>
+                <div className='introduction'>
+                    <h2 className='greetings'>Hello</h2>
+                    <h2 className='intro'>I am Amir Maharjan</h2>
+                    <span className='profession'>I am a Web Developer</span>
+                </div>
+                <figure className='portfolio-thumb'>
+                    <img src={Astronaut} />
+                </figure>
+            </div>
         </>
     );
 }
--- react-official/src/Components/Menu.js
+++ react-official/src/Components/Menu.js
@@ -1,28 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 export default function Menu () {
+    const [ headerItem, setHeaderItem ] = useState('header-item');
+
+    // live search
+    const LiveSearch = () => {
+        return (
+            <>
+                <div className={ headerItem + ' live-search'}>
+                    <input type="text" placeholder='Search ...' />
+                    <button>Search</button>
+                </div>
+            </>
+        );
+    }
+    
+    // logo
+    const Logo = () => {
+        return (
+            <>
+                <span className={ headerItem + ' logo'} onClick={handleMenuItem}><Link to="/">LOGO.</Link></span>
+            </>    
+        );
+    }
+
+    // Menu return
+    const MenuItem = ( props ) => {
+        return <li className={ 'menu-item ' + props.mainClass} onClick={ handleMenuItem }><Link to={ props.link }>{ props.label }</Link></li>
+    }
+
+    // handleMenuItem
+    const handleMenuItem = ( event ) => {
+        let menuItemArray = document.getElementsByClassName('menu-item')
+        Array.from(menuItemArray).forEach(function( currentValue ){
+            currentValue.classList.remove('isActive')
+        })
+        if( ! event.target.parentNode.classList.contains('menu-item') ) {
+            menuItemArray[0].classList.add('isActive')
+            return
+        }
+        event.target.parentNode.classList.add('isActive')
+    }
+
+    // Unique key function
+    const uniqueKey = () => {
+        let menuItem = 4, keyArr = [], linkArr = ['/', '/games', '/about', '/practice'], labelArr = ['Home', 'Games', 'About', 'Practice']
+        for( let i = 0; i < menuItem; i++ ) {
+            keyArr.push({ key: i, link: linkArr[i], label: labelArr[i] })
+        }
+        return keyArr
+    }
+
     return (
         <>
             <div className='nav-menu'>
-                <ul className='menu-list'>
-                    <li className='menu-item'><Link to="/">Home</Link></li>
-                    <li className='menu-item'><Link to="/games">Games</Link></li>
-                    <li className='menu-item'><Link to="/about">About</Link></li>
-                    <li className='menu-item'><Link to="/practice">Practice</Link></li>
+                <Logo />
+                <ul className={ headerItem + ' menu-list'}>
+                    { uniqueKey().map((element) => <MenuItem key={element.key} mainClass={ (element.key == 0) ? 'isActive' : ''} link={element.link} label={element.label}/>) }
                 </ul>
                 <LiveSearch/>
-            </div>
-        </>
-    );
-}
-
-const LiveSearch = () => {
-    return (
-        <>
-            <div className='live-search'>
-                <input type="text" placeholder='Search ...' />
-                <button>Search</button>
             </div>
         </>
     );
