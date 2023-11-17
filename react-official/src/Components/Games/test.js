a86c207d79ddade9d889805175d052666195049a
Amir Maharjan <mhrznamir.am@gmail.com>
2023-11-17 14:58:32 +0545

    testing

---
+++ react-official/src/Components/Games/test.js
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from 'react'
+
+export default function TicTacToe () {
+    const [ action, setAction ] = useState(null)
+    const [ showGameboard, setShowGameboard ] = useState(false)
+    const handleActionClick = (event, type) => {
+        setAction(type)
+        if( type == 'start' ) setShowGameboard( true )
+        if( type == 'end' ) setShowGameboard( false )
+    }
+    return (
+        <>
+            <div className='tic-tac-toe-wrap'>
+                <h1>Welcome to TicTacToe</h1>
+                <div className='game-actions'>
+                    <GameAction label="Start" onTouch={(event) => handleActionClick(event, 'start') } />
+                    <GameAction label="Reset" onTouch={(event) => handleActionClick(event, 'reset') } />
+                    <GameAction label="End" onTouch={(event) => handleActionClick(event, 'end') } />
+                </div>
+                {showGameboard && <GameBoard />}
+            </div>
+        </>    
+    );
+}
+
+const GameAction = ( { label, onTouch } ) => {
+    let lowerCaseLabel = label.toLowerCase()
+    let thisClass 
+    if( lowerCaseLabel == 'start' ) thisClass = 'start-game'
+    if( lowerCaseLabel == 'reset' ) thisClass = 'reset-game'
+    if( lowerCaseLabel == 'end' ) thisClass = 'end-game'
+    return (
+        <span className={'game-action ' + thisClass} onClick={ onTouch }>{label}</span>    
+    );
+}
+
+const GameBoard = () => {
+    const [ string, setString ] = useState()
+    const [ count, setCount ] = useState(0)
+    const TableRow = () => {
+        return (
+            <tr>
+                <TableColumn/>
+                <TableColumn/>
+                <TableColumn/>
+            </tr>    
+        );
+    }
+
+    const TableColumn = () => {
+        const handleCellClick = (event) => {
+            console.log(  )
+            if( count % 2 == 0 ) event.target.innerHTML = 'X'
+            if( count % 2 != 0 ) event.target.innerHTML = 'O'
+            setCount( count+1 )
+        }
+        return (
+            <td className='column-elements' onClick={ handleCellClick }></td>
+        );
+    }
+
+    return (
+        <>
+            <table className='gameboard'>
+                <tbody>
+                    <TableRow />
+                    <TableRow />
+                    <TableRow />
+                </tbody>
+            </table>
+        </>    
+    );
+}
