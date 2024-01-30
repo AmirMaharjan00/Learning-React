22b5cfe71e1175dc8c79035e807882486d6f33f7
Amir Maharjan <mhrznamir.am@gmail.com>
2024-01-30 09:52:41 +0545

    editor css

--- shop-swiftly/src/components/admin/editor.js
+++ shop-swiftly/src/components/admin/editor.js
@@ -11,17 +11,23 @@ export default function Editor( { editorClose } ) {
     return (
         <>
             <div className='swt-admin-editor' id='swt-admin-editor'>
+                <span className='editor-close' onClick={ () => { editorClose() } }>X</span>
                 <form onSubmit={ handleFormSubmit } >
                     <div className='editor-area'>
                         <div className='editor-main'>
-                            Editor Area
+                            <input type='text' placeholder='Title' name='product_name' id='product_name' />
+                            <textarea placeholder='Description' name='product_description' id='product_description'></textarea>
                         </div>
                         <div className='editor-sidebar'>
-                            Editor Sidebar
+                            <button className='editor-submit'>Publish</button>
+                            <div className='sidebar-elements-wrap'>
+                                <div className='sidebar-element'>Categories</div>
+                                <div className='sidebar-element'>Tags</div>
+                                <div className='sidebar-element'>Featured Image</div>
+                            </div>
                         </div>
                     </div>
                 </form>
-                <span className='editor-close' onClick={ () => { editorClose() } }>X</span>
             </div>
         </>
     );
